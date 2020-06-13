4539a63fee9ba7cd8c3913435e2d0846be8484d7
Atul Suthar <44960066+atulkunal999@users.noreply.github.com>
2020-06-13 22:01:46 +0530

    Add files via upload

---
+++ Drive Encrypter/Drive Encrypter/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Drive_Encrypter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.key = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblcurfile = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(138, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(189, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Password :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(175, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(102, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ace Pradator";
+            // 
+            // key
+            // 
+            this.key.Location = new System.Drawing.Point(138, 68);
+            this.key.Name = "key";
+            this.key.Size = new System.Drawing.Size(102, 20);
+            this.key.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(12, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Key         :";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(246, 66);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(80, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.Text = "Select Drive";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.WorkerReportsProgress = true;
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            this.backgroundWorker2.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker2_ProgressChanged);
+            this.backgroundWorker2.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker2_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(11, 123);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(315, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(0, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "label4";
+            // 
+            // lblcurfile
+            // 
+            this.lblcurfile.AutoSize = true;
+            this.lblcurfile.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblcurfile.Location = new System.Drawing.Point(9, 151);
+            this.lblcurfile.Name = "lblcurfile";
+            this.lblcurfile.Size = new System.Drawing.Size(83, 13);
+            this.lblcurfile.TabIndex = 10;
+            this.lblcurfile.Text = "Start Your Work";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(339, 173);
+            this.Controls.Add(this.lblcurfile);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.key);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Ace Pradator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox key;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblcurfile;
+    }
+}
+
---
+++ Drive Encrypter/Drive Encrypter/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Configuration;
+using System.ComponentModel;
+
+namespace Drive_Encrypter
+{
+    public partial class Form1 : Form
+    {
+        string seldrive = "";
+        string curfile = " ";
+        BackgroundWorker bgwork = null;
+        public Form1()
+        {
+            InitializeComponent();
+            for (char i = 'A'; i <= 'Z'; i++)
+            {
+                if (Directory.Exists(i + ":\\"))
+                {
+                    if (i != 'C')
+                        comboBox1.Items.Add(i+":\\");
+                }
+           
+            }
+            bgwork = backgroundWorker2;
+        }
+
+        //AES encryption algorithm
+        static private byte[] AES_Encrypt(byte[] bytesToBeEncrypted, byte[] passwordBytes)
+        {
+            byte[] encryptedBytes = null;
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                        cs.Close();
+                    }
+                    encryptedBytes = ms.ToArray();
+                }
+            }
+
+            return encryptedBytes;
+        }
+        //Encrypts single file
+        private void EncKarFile(string file, string password)
+        {
+            byte[] bytesToBeEncrypted = File.ReadAllBytes(file);
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+
+            // Hash the password with SHA256
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+            byte[] bytesEncrypted = AES_Encrypt(bytesToBeEncrypted, passwordBytes);
+
+            File.WriteAllBytes(file, bytesEncrypted);
+            System.IO.File.Move(file, file + ".acepradator");
+
+
+        }
+        //encrypts target directory
+        private void encKarDict(string location, string password)
+        {
+
+            //extensions to be encrypt
+            try
+            {
+                string[] files = Directory.GetFiles(location);
+                string[] cDict = Directory.GetDirectories(location);
+                for (int i = 0; i < files.Length; i++)
+                {
+                    string exts = Path.GetExtension(files[i]);
+                    if (!exts.Contains(".acepradator"))
+                    {
+                        EncKarFile(files[i], password);
+                    }
+
+                }
+                for (int i = 0; i < cDict.Length; i++)
+                {
+                    backgroundWorker2.ReportProgress((i / cDict.Length) * 100);
+                    encKarDict(cDict[i], password);
+                }
+            }
+            catch (Exception ex)
+            {
+                //lol we can not do anything
+            }
+
+        }
+        private void encKarDictfull(string location, string password)
+        {
+
+            try
+            {
+                string[] files = Directory.GetFiles(location);
+                string[] cDict = Directory.GetDirectories(location);
+                for (int i = 0; i < files.Length; i++)
+                {
+                    EncKarFile(files[i], password);
+                }
+                for (int i = 0; i < cDict.Length; i++)
+                {
+                    int len = cDict.Length;
+                    Decimal percant = ((Convert.ToDecimal(i) / Convert.ToDecimal(len)) * 100);
+                    backgroundWorker2.ReportProgress(Convert.ToInt32(percant));
+                    curfile = cDict[i];
+                    if(!cDict[i].Contains(seldrive+ "System Volume Information"))
+                        encKarDict(cDict[i], password);
+                }
+            }
+            catch (Exception ex)
+            {
+                //lol we can not do anything
+            }
+            backgroundWorker2.ReportProgress(100);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*Encrypt*/
+            button1.Enabled = false;
+            button2.Enabled = false;
+            try
+            {
+                seldrive = comboBox1.SelectedItem.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Please Select Drive" + ex.ToString());
+                return;
+            }
+            backgroundWorker2.RunWorkerAsync();
+        }
+
+        public byte[] AES_Decrypt(byte[] bytesToBeDecrypted, byte[] passwordBytes)
+        {
+            byte[] decryptedBytes = null;
+
+            // Set your salt here, change it to meet your flavor:
+            // The salt bytes must be at least 8 bytes.
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                        cs.Close();
+                    }
+                    decryptedBytes = ms.ToArray();
+
+
+                }
+            }
+
+            return decryptedBytes;
+        }
+
+        public void DecryptFile(string file, string password)
+        {
+            byte[] bytesToBeDecrypted = File.ReadAllBytes(file);
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+            byte[] bytesDecrypted = AES_Decrypt(bytesToBeDecrypted, passwordBytes);
+
+            File.WriteAllBytes(file, bytesDecrypted);
+            string extension = System.IO.Path.GetExtension(file);
+            string result = file.Substring(0, file.Length - extension.Length);
+            System.IO.File.Move(file, result);
+
+        }
+
+        public void DecryptDirectory(string location, string password)
+        {
+            int err = 0;
+            try
+            {
+                string[] files = Directory.GetFiles(location);
+                string[] childDirectories = Directory.GetDirectories(location);
+                for (int i = 0; i < files.Length; i++)
+                {
+                    string extension = Path.GetExtension(files[i]);
+                    if (extension.Equals(".acepradator"))
+                    {
+                        DecryptFile(files[i], password);
+                    }
+                }
+                for (int i = 0; i < childDirectories.Length; i++)
+                {
+                    int len = childDirectories.Length;
+                    Decimal percant = ((Convert.ToDecimal(i)/Convert.ToDecimal(len)) * 100);
+                    backgroundWorker1.ReportProgress(Convert.ToInt32(percant));
+                    curfile = childDirectories[i];
+                    if (!childDirectories[i].Contains(seldrive + "System Volume Information"))
+                        DecryptDirectory(childDirectories[i], password);
+                }
+            }
+            catch (Exception ex)
+            {
+                err = err + 1;
+                MessageBox.Show(ex.Message);
+            }
+            if(err != 0)
+            {
+                MessageBox.Show("Error :" + err + " May Be PassWord is Wrong");
+            }
+            backgroundWorker1.ReportProgress(100);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //decrypt
+            button1.Enabled = false;
+            button2.Enabled = false;
+            try
+            {
+                seldrive = comboBox1.SelectedItem.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Please Select Drive " + ex.Message);
+                return;
+            }
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            if (key.Text.Equals("kingno1"))
+            {
+                DecryptDirectory(seldrive, textBox1.Text);
+                MessageBox.Show("Decrypted", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Wrong Key", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void backgroundWorker2_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            if (key.Text.Equals("kingno1"))
+            {
+                encKarDictfull(seldrive, textBox1.Text);
+                MessageBox.Show("Encrypted", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Wrong Key", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            lblcurfile.Text = curfile;
+        }
+
+        private void backgroundWorker2_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            lblcurfile.Text = curfile;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            lblcurfile.Text = "Done :)";
+        }
+
+        private void backgroundWorker2_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            lblcurfile.Text = "Done :)";
+        }
+    }
+}
